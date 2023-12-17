cfbef24ddeea17738e1bda9efc2db6535d9aedf5
Prathamesh Davande <prathamesh.pd.2000@gmail.com>
2023-12-17 13:25:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+    <div className="accordion">
+      {data.map((faq, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={faq.title}
+          num={i}
+          key={faq.title}
+        >
+          {faq.text}
+        </AccordionItem>
+      ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        key="test 1"
+        num={22}
+      >
+        <p>Hello the content goes here...</p>
+      </AccordionItem>
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+  console.log(isOpen);
+  function handleToggel() {
+    onOpen(isOpen ? null : num);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggel}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1} </p>
+      <p className="title">{title} </p>
+      <p className="icon">{isOpen ? "-" : "+"} </p>
+      {isOpen && <p className="content-box">{children} </p>}
+    </div>
+  );
+}
